e84d201fa9108dfa7d95dbd9c4df5818c2a52991
Jory Courtney <jory2189@gmail.com>
2022-09-19 16:12:58 -0400

    commit finish

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -2,20 +2,8 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 public class PositionType extends JobField {
-
-
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public PositionType() {
-        id = nextId;
-        nextId++;
-    }
-
     public PositionType(String value) {
-        this();
-        this.value = value;
+      super(value);
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
@@ -25,17 +13,6 @@ public class PositionType extends JobField {
 
     // Getters and Setters:
 
-    public int getId() {
-        return id;
-    }
 
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-        super(value);
-    }
 
 }
